8c0af24e7a35412a2ffd1569643eddf67f654350
Daniel Kats <d.kats@fkf.mpg.de>
2018-02-01 10:22:51 +0100

    Handle arguments, define delimiters for the output
    csvfwf [-d "<delimiter>"] <inputfile>

--- csvfwf.py
+++ csvfwf.py
@@ -1,44 +1,61 @@
 #!/usr/bin/python3
 
-import numpy as np
-import pandas as pd
+import sys
+from sys import stdout, argv, stderr
+import pandas
 
-df = pd.read_csv("test1.csv")
+#default delimiter
+delimiter=","
 
-cols = df.columns
-vals = df.values
-table = []
-# find the optimal width for each column
-for icol in range(len(cols)):
-  col = cols[icol].strip()
-  if col.startswith("Unnamed:") or col == "nan":
-    col = ""
-  maxlen=len(col)
-  #print(col,maxlen)
-  for irow in range(len(vals)):
-    val=str(vals[irow][icol])
-    val=val.strip()
-    itlen=len(val)
-    if itlen > maxlen:
-      maxlen = itlen
-    #print("*",val,"*")
-  #print("maxlen=",maxlen)
-  maxlen += 1
-  fcol=[]
-  fcol.append('{:>{}}'.format(col,maxlen))
-  for irow in range(len(vals)):
-    val=str(vals[irow][icol])
-    val=val.strip()
-    if val.startswith("Unnamed:") or val == "nan":
-      val = ""
-    fcol.append('{:>{}}'.format(val,maxlen))
-  #print(fcol)
-  table.append(fcol)
-#output
-for irow in range(len(vals)+1):
+def RemovePandasNan(val):
+  """resets values set by pandas to empty space again"""
+  if val.startswith("Unnamed:") or val == "nan":
+    val = ""
+  return val
+
+def FixWidth(df):
+  """generate a table with fixed width columns"""
+  cols = df.columns
+  vals = df.values
+  table = []
   for icol in range(len(cols)):
-    print(table[icol][irow],",",end="",sep="")
-  print()
+    col = cols[icol].strip()
+    col=RemovePandasNan(col)
+    #find the optimal width for each column
+    maxlen=len(col)
+    for irow in range(len(vals)):
+      val=str(vals[irow][icol])
+      val=val.strip()
+      val=RemovePandasNan(val)
+      vals[irow][icol]=val
+      itlen=len(val)
+      if itlen > maxlen:
+        maxlen = itlen
+    maxlen += 1
+    fcol=[]
+    fcol.append('{:>{}}'.format(col,maxlen))
+    for irow in range(len(vals)):
+      fcol.append('{:>{}}'.format(vals[irow][icol],maxlen))
+    table.append(fcol)
+  return table
 
-#print(table)
+def PrintTable(table):
+  for irow in range(len(table[0])):
+    for icol in range(len(table)):
+      print(table[icol][irow],delimiter,end="",sep="")
+    print()
+
+ArgsLoop = iter(sys.argv[1:]) 
+for Arg in ArgsLoop:
+  if Arg.startswith("-"):
+    if Arg.startswith("-d"): 
+      Arg = next(ArgsLoop)
+      delimiter = Arg
+    else:
+      print("option "+Arg+" not known") 
+  else:
+    CSVFile = Arg
+    df = pandas.read_csv(CSVFile)
+    table = FixWidth(df)
+    PrintTable(table)
 
